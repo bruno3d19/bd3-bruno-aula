563b2c96c5fef793d17d191125609d7ef156947d
bruno3d19 <brunorodriguesdossantos16@gmail.com>
2024-08-27 20:36:56 -0300

    tipos de seleção

---
+++ find_where.fields.mongodb.js
@@ -0,0 +1,13 @@
+// NOME DO BANCO DE DADOS
+const database = 'BD3-BRUNO-AULA';
+
+// NOME DO BANCO DA COLEÇÃO DE DADOS
+const collection = 'LIVRARIA';
+
+//CRIAR OU ACESSAR O BANCO DE DADOS
+use(database);
+
+db[collection].find(
+                        {"categoria":"Ficção Científica"},
+                        {"_id":0, "codigo":0}
+                    );
